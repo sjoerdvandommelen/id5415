1c0c0d714cd883dd2449198020773dc9f698a5e6
Jacky Bourgeois <J.Bourgeois@tudelft.nl>
2020-09-03 13:15:25 +0200

    fix script search

--- src/js/simpleJekyllSearch.js
+++ src/js/simpleJekyllSearch.js
@@ -3,7 +3,7 @@
         var settings = $.extend({
             jsonFile        : location+'/search.json',
             jsonFormat      : 'title,tags,categories,url,date',
-            template : '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{date}</time></span></a></article></li>',
+            template : '<li><article><a href="{url}">{title}</a></article></li>',
             searchResults   : '.search-results',
             searchResultsTitle   : '<h4>Search results:</h4>',
             limit           : '10',
